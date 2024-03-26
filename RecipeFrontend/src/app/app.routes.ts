8d5e5dc9d59bd4b783718cb3be72eb74ca382426
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-03-26 10:43:35 +0200

    Basic routing

--- RecipeFrontend/src/app/app.routes.ts
+++ RecipeFrontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:RecipesComponent},
+    {path:'recipe:id', component:RecipeComponent},
+    {path:'profile', component:ProfileComponent}
+];
