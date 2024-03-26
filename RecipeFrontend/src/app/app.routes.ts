821edf38fa029403d7498d0587a8a5321685ec76
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-03-26 10:49:59 +0200

    login route

--- RecipeFrontend/src/app/app.routes.ts
+++ RecipeFrontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path:'', component:RecipesComponent},
     {path:'recipe:id', component:RecipeComponent},
-    {path:'profile', component:ProfileComponent}
+    {path:'profile', component:ProfileComponent},
+    {path:'login', component:LoginComponent}
 ];
